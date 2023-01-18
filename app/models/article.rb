8f09122eb6470f597a2fb0826f1ab87ee5ef440f
Alex <83082486+aleksandar-code@users.noreply.github.com>
2023-01-18 18:09:11 +0100

    Update model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
-    validates :title, presence: true
-    validates :body, presence: true, length: { minimum: 10 }
+  has_many :comments
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
   end
   
